df1cb95e6d378fff396dd598441d5a7757d77759
Olin Kirkland <me@olinkirk.land>
2025-10-17 20:51:08 -0400

    Monorepo deploy w/o issues

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { URL } from 'node:url';
 
-// https://vite.dev/config/
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    plugins: [vue()],
+    server: { host: true }, // For external IP access
+    // When localhost, make base '/', otherwise '/flag-game/'
+    base: process.env.NODE_ENV === 'development' ? '/' : '/flags/',
+    resolve: {
+        alias: {
+            '@': new URL('./src', import.meta.url).pathname
+        }
+    }
+});
