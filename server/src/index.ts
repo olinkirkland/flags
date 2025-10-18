3a43ec410743ea107d9dbe5a9d6d042ab4290822
Olin Kirkland <me@olinkirk.land>
2025-10-17 20:36:29 -0400

    Initialize rest server, add log request, start scaffolding

---
+++ server/src/index.ts
@@ -0,0 +1,38 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import { createServer } from 'http';
+import logRequest from './middleware/log-request';
+
+export const origin = ['http://localhost:5173', 'https://olinkirk.land/flags'];
+
+main();
+
+async function main() {
+    dotenv.config();
+
+    // Initialize REST server
+    const app = express();
+
+    app.use(express.json());
+    app.use(logRequest);
+    app.use(
+        cors({
+            origin,
+            credentials: true
+        })
+    );
+
+    // TODO: Add routes
+
+    const server = createServer(app);
+
+    // TODO: Add the SocketIO server
+    // const socketIOServer;
+
+    // Start the server
+    const port = process.env.PORT || 3000;
+    server.listen(port, () => {
+        console.log('Server running on port', port);
+    });
+}
